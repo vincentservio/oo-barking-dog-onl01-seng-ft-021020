1c8500ecdae3db0c9c8033b955c7d115275086e8
Vincent Servio <vincentservio@gmail.com>
2020-02-17 09:08:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,12 @@ class Dog
    puts "woof!"
   end
   def name=(dog_name)
-    the_dogs_name = dog_name
+    @the_dogs_name = dog_name
     
   end
- 
+  def name 
+    @the_dogs_name
+  end
 end 
 
 fido = Dog.new
